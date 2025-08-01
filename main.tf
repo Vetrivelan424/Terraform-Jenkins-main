a07069e829a2cbd5f436e17345a8b1747e0c88ca
Vetrivelan M <96474364+Vetrivelan424@users.noreply.github.com>
2025-08-01 12:53:13 +0530

    tier chnage
    tier chnage

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "foo" {
   ami           = "ami-0c2b8ca1dad447f8a" # Amazon Linux 2 in us-east-1
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "TF-Instance"
